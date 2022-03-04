9e3ef481d44eb91b11d74fb28855185dd324103d
HiroMuraki <419734782@qq.com>
2022-03-04 12:59:34 +0800

    fix resoure

--- make.ps1
+++ make.ps1
@@ -3,4 +3,4 @@ $hasPath = Test-Path "OutputProgram/DEModLauncher.exe"
 if ($hasPath) {
     Remove-Item "OutputProgram/DEModLauncher.exe"
 }
-Copy-Item "C:\Users\11717\source\repos\DEModLauncher_GUI\DEModLauncher_GUI\bin\Release\net5.0-windows\win10-x64\publish\DEModLauncher_GUI.exe" "OutputProgram/DEModLauncher.exe"
+Copy-Item "C:\Users\11717\source\repos\DEModLauncher_GUI\DEModLauncher_GUI\bin\Release\net6.0-windows\win10-x64\publish\DEModLauncher_GUI.exe" "OutputProgram/DEModLauncher.exe"
